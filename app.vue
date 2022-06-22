3907709bb4896176ce68a3b8446b97ad443da018
junichi-chiba <tanquar.g@gmail.com>
2022-06-22 14:29:20 +0900

    install tailwind

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div>
+    <div class="text-green-500 text-lg font-semibold px-4 py-1 bg-zinc-100">
       This is a Nuxt3 + TailwindCSS + MongoDB example.
     </div>
     <NuxtWelcome />
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports =
+  {
+    content: [
+      "./app.vue",
+      "./components/**/*.{vue,js,ts}",
+      "./layouts/**/*.{vue,js,ts}",
+      "./pages/**/*.{vue,js,ts}",
+      "./plugins/**/*.{js,ts}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
