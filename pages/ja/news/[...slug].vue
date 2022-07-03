03f7371c92ffcbc4cf0f91b8fd582fa8158d5e8c
junichi-chiba <tanquar.g@gmail.com>
2022-07-03 23:34:11 +0900

    add md sample

---
+++ pages/ja/news/[...slug].vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <ul class="mx-4 list-disc">
+          <NuxtLink to="/">Top</NuxtLink>
+          &gt;
+          <NuxtLink to="/ja">Ja</NuxtLink>
+          &gt;
+          <NuxtLink to="/ja/hello">Hello</NuxtLink>
+        </ul>
+      </nav>
+    </header>
+
+    <main>
+      <ContentDoc class="px-4 py-4 mx-4 my-4 border rounded bg-slate-100"/>
+    </main>
+  </div>
+</template>
