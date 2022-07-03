c4b37283dbfc924b7ae3496fb83ff94f59f19175
junichi-chiba <tanquar.g@gmail.com>
2022-07-03 22:36:40 +0900

    refine example and readme

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="container px-4 mx-auto my-4">
-    <h1 class="text-xl text-gray-800">
-      Welcome!
-    </h1>
-    <Button outline>Click me</Button>
-    <p>
-      Choose locale
-    </p>
-    <ul class="bullet">
-      <li><NuxtLink to="/ja">ja</NuxtLink></li>
-      <li><NuxtLink to="/en">en</NuxtLink></li>
-    </ul>
+  <div>
+    <main class="container px-4 mx-auto my-4">
+      <h1 class="text-xl text-gray-800">
+        Welcome!
+      </h1>
+      <Button outline>Click me</Button>
+      <p>
+        Choose locale
+      </p>
+      <ul class="mx-4 list-disc">
+        <li><NuxtLink to="/ja">ja</NuxtLink></li>
+        <li><NuxtLink to="/en">en</NuxtLink></li>
+      </ul>
+    </main>
   </div>
 </template>
