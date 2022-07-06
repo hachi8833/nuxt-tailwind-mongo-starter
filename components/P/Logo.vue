d0580baec3e2d275581ef6a18fddf20f13da9473
hachi8833 <hachi8833@gmail.com>
2022-07-06 18:34:38 +0900

    Add corporate logo

--- components/P/Logo.vue
+++ components/P/Logo.vue
@@ -1,11 +1,7 @@
 <template>
   <!-- logo - start -->
   <a href="/" class="inline-flex items-center text-black-800 text-2xl md:text-3xl font-bold gap-2.5" aria-label="logo">
-    <svg width="95" height="94" viewBox="0 0 95 94" class="w-6 h-auto text-indigo-500" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-      <path d="M96 0V47L48 94H0V47L48 0H96Z" />
-    </svg>
-
-    Flowrift
+  <img src="~/assets/images/top_logo_kon.jpeg" alt="atglobal logo">
   </a>
   <!-- logo - end -->
 </template>
