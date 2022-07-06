590e3d609025b49e16ed4b38a0d96eb43415ec7c
hachi8833 <hachi8833@gmail.com>
2022-07-06 18:47:25 +0900

    Add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,5 +24,17 @@ export default defineNuxtConfig({
       locale: 'ja',
       fallbackLocale: [ 'ja', 'en' ],
     }
+  },
+  app:{
+    head: {
+      title: "アットグローバル | 多言語コンサル・プロモーションサポート: カタログ翻訳からハード・ソフトウェアマニュアル翻訳まで様々な分野の技術翻訳に対応します",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+      link: [
+        { rel: "icon", type: "image/png", href: "/favicon.png" },
+      ],
+    },
   }
 })
