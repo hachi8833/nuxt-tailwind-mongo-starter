533e4fc007d1362bc5137235b9e6c198a1ab1181
hachi8833 <hachi8833@gmail.com>
2022-07-07 16:38:06 +0900

    Add privacy policy page

---
+++ pages/privacy.vue
@@ -0,0 +1,108 @@
+<template>
+ <div class="bg-white py-6 sm:py-8 lg:py-12">
+  <div class="px-4 md:px-8 mx-16">
+    <h1 class="text-gray-800 text-2xl sm:text-3xl font-bold text-center mb-4 md:mb-6">個人情報保護方針</h1>
+
+    <p class="text-gray-500 sm:text-lg mb-6 md:mb-8">
+      株式会社アットグローバル（以下「当社」といいます）は、業務上の個人情報（個人情報の保護に関する法律第2条第1項）に関し、同法の定めに基づき、以下の点についてお知らせいたします。
+    </p>
+
+    <h2 class="text-gray-800 text-xl sm:text-2xl font-bold mb-4 md:mb-6">1．利用目的</h2>
+
+    <h3 class="text-gray-800 text-lg sm:text-xl font-bold mb-4 md:mb-6">（1）お客様の個人情報</h3>
+
+    <ul class="list-disc m-6">
+      <li>受託業務の実施、成果物等の発送</li>
+      <li>翻訳成果物等の発送、お預かりした資料等の返却その他のサービスの提供に必要なご連絡</li>
+      <li>対価の徴収、債権債務の管理、顧客情報の管理</li>
+      <li>お問い合わせ・ご相談等への対応、確認および記録</li>
+      <li>お客様サポート、アフターサービス等の対応</li>
+      <li>サービス向上、業務効率化施策の実施</li>
+      <li>当社サービスの企画・開発・改善</li>
+      <li>当社業務に関する調査研究、データ収集・分析</li>
+    </ul>
+
+    <h3 class="text-gray-800 text-lg sm:text-xl font-bold mb-4 md:mb-6">（2）翻訳者・校正者その他業務委託先の個人情報</h3>
+
+    <ul class="list-disc m-6">
+      <li>業務委託先の選考、登録、管理、業務上必要な諸連絡</li>
+      <li>翻訳・レビュー・マネジメント業務の手配、契約の締結、委託料の支払等契約の履行</li>
+      <li>債権債務の管理、税務処理その他の必要な手続</li>
+    </ul>
+
+    <h3 class="text-gray-800 text-lg sm:text-xl font-bold mb-4 md:mb-6">（3）採用・募集活動応募者、退職者の個人情報</h3>
+
+    <ul class="list-disc m-6">
+      <li>採用・募集活動応募者への情報提供および連絡、その他採用・募集活動に関連する業務</li>
+      <li>退職後の諸手続き、退職者への情報提供および連絡等</li>
+    </ul>
+
+    <h3 class="text-gray-800 text-lg sm:text-xl font-bold mb-4 md:mb-6">（4）社員およびその家族等の個人情報</h3>
+
+    <ul class="list-disc m-6">
+      <li>雇用管理に関する業務</li>
+      <li>給与等の支払いに関する業務</li>
+      <li>福利厚生業務</li>
+      <li>法令上必要な諸手続きおよび連絡等</li>
+      <li>上記の他、業務上必要な諸手続きおよび連絡等</li>
+    </ul>
+
+    <h3 class="text-gray-800 text-lg sm:text-xl font-bold mb-4 md:mb-6">（5）特定個人情報の利用目的</h3>
+
+    <ul class="list-disc m-6">
+      <li>健康保険および雇用保険等に関する事務</li>
+      <li>厚生年金および国民年金等に関する事務</li>
+      <li>給与、賞与および年末調整等、所得税源泉徴収等に関する事務</li>
+      <li>源泉徴収票、給与支払報告書等の作成および提出に関する事務</li>
+      <li>法定の支払調書等の作成および提出に関する事務のため</li>
+      <li>上記の他、番号法等に定める事務のため</li>
+    </ul>
+
+    <h2 class="text-gray-800 text-xl sm:text-2xl font-bold mb-4 md:mb-6">2．個人情報の第三者提供</h2>
+
+    <p class="text-gray-500 sm:text-lg mb-6 md:mb-8">
+      当社は下記の場合を除き、上記の個人情報を第三者に提供しません。
+    </p>
+
+    <ul class="list-disc m-6">
+      <li>ご本人の同意がある場合</li>
+      <li>法令に基づく場合</li>
+      <li>人の生命、身体又は財産の保護のために必要であって、ご本人の同意を取ることが困難である場合</li>
+      <li>合併その他の理由による事業の承継に伴い、個人情報を提供する場合</li>
+      <li>国の機関もしくは地方公共団体又はその委託を受けた者が法令の定める事務を遂行することに対して協力する必要がある場合に、ご本人の同意を得ることにより当該事務の遂行に支障を及ぼすおそれがある場合</li>
+    </ul>
+
+    <p class="text-gray-500 sm:text-lg mb-6 md:mb-8">
+      当社は、「1．個人情報の利用目的」に掲げる利用目的の達成に必要な範囲内において、個人情報の取り扱いの全部または一部を委託する場合があります。この場合、当社は、個人情報の取扱いに関する契約を締結するなど委託先に対する必要かつ適切な監督を行います。
+    </p>
+
+    <p class="text-gray-500 sm:text-lg mb-6 md:mb-8">
+      ご本人であることを確認した後、ご請求のあった個人情報の開示等に対する回答内容を、書面にてご本人または代理人のご住所宛の郵便により通知いたします。また、正当な理由により開示等を行わない場合は、その理由を付して郵便により通知いたします。
+    </p>
+
+    <h2 class="text-gray-800 text-xl sm:text-2xl font-bold mb-4 md:mb-6">3．属性情報・行動履歴の取得および利用について</h2>
+
+    <p class="text-gray-500 sm:text-lg mb-6 md:mb-8">
+      当社GDPRプライバシーポリシーに規定しています。
+    </p>
+
+    <h2 class="text-gray-800 text-xl sm:text-2xl font-bold mb-4 md:mb-6">4．変更</h2>
+
+    <p class="text-gray-500 sm:text-lg mb-6 md:mb-8">
+      当社は、法令その他の変更等に対応する等　必要に応じて、この個人情報の取り扱いを予告なく変更する場合があります。
+    </p>
+
+    <h2 class="text-gray-800 text-xl sm:text-2xl font-bold mb-4 md:mb-6">お問い合わせ先</h2>
+
+    <p class="text-gray-500 sm:text-md mb-6 md:mb-8">
+      hiroichi.goto@atglobal.co.jp<br>
+      後藤裕一（コンプライアンス担当）
+    </p>
+
+    <p class="text-gray-500 sm:text-md mb-6 md:mb-8">
+      2019年11月1日改訂
+    </p>
+
+  </div>
+</div> 
+</template>
