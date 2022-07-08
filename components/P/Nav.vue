89c92884d811eefa4340a0126d9989723481e7d4
hachi8833 <hachi8833@gmail.com>
2022-07-08 11:01:39 +0900

    Add nav links via setup script

--- components/P/Nav.vue
+++ components/P/Nav.vue
@@ -1,11 +1,20 @@
 <template>
   <!-- nav - start -->
   <nav class="hidden lg:flex gap-12 2xl:ml-16">
-    <a href="#" class="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-sm font-semibold transition duration-100">翻訳サービス</a>
-    <a href="#" class="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-sm font-semibold transition duration-100">海外・国内リサーチ</a>
-    <a href="#" class="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-sm font-semibold transition duration-100">デジタルマーケティング</a>
-    <a href="#" class="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-sm font-semibold transition duration-100">海外ビジネスコンサル</a>
-    <a href="#" class="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-sm font-semibold transition duration-100">会社概要</a>
-  </nav>
+    <template v-for="item of menuItems">
+      <a :href="item.href" class="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-sm font-semibold transition duration-100">
+        {{ item.title }}
+      </a>
+    </template>
   <!-- nav - end -->
+  </nav>
 </template>
+
+<script setup>
+const menuItems = [
+  { href: 'translation', title: '翻訳サービス' },
+  { href: 'research', title: '海外・国内リサーチ' },
+  { href: 'marketing', title: 'デジタルマーケティング' },
+  { href: 'consulting', title: '海外ビジネスコンサル' },
+]
+</script>
